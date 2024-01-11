240a3d716952da688ab1ec5a65417384a5c5c5c8
Szabó Roland (SZF_2023_2025) <szabo.roland1@diak.szamalk-szalezi.hu>
2024-01-11 15:35:51 +0100

    Save

--- funk.js
+++ funk.js
@@ -2,4 +2,28 @@
     // Kinézet: Képek, színek jók: 10
     // Elrendezés, reszponzivitás: Van mit javítani rajta: 3
     // kód: 
-    // 
+// Halász Gergő, Baksa Bence
+    // Kinézet: Jó: 10
+    // Téma: 9.5 (Foci)
+// Szalkai Viktória Csenge, Roszkopf Léna Adél
+    // Előadás: Jó: 10 Elakadt: 4
+    // Kinézet: Kiégeti a szemem. 0
+    // Funkciók: Jók a slide-ok.: 9
+// Kocsi Dorottya, Csorba Eszter Katinka:
+    // Kinézet: Tökéletes
+    // Elrendezés: Tökéletes
+    // Téma: Tökéletes
+// Richter Balázs, Weimper Gergő Zsombor
+    // Elrendezés: Jó
+    // // Kinézet: Van mit javítani rajta
+
+videoVisible = false
+function toggleVideoVisible(){
+    videoVisible = !videoVisible;
+    if (videoVisible){
+        document.getElementById("hal").style.display = 'none';
+    }
+    else{
+        document.getElementById("hal").style.display = 'block';
+    }
+}
