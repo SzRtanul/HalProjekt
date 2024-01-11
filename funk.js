299e608b21927ed2a8e4481871a0e8fb63455632
Szabó Roland (SZF_2023_2025) <szabo.roland1@diak.szamalk-szalezi.hu>
2024-01-11 15:54:08 +0100

    Save

--- funk.js
+++ funk.js
@@ -2,28 +2,46 @@
     // Kinézet: Képek, színek jók: 10
     // Elrendezés, reszponzivitás: Van mit javítani rajta: 3
     // kód: 
+        // Karácsony
 // Halász Gergő, Baksa Bence
     // Kinézet: Jó: 10
     // Téma: 9.5 (Foci)
+        // Foci
 // Szalkai Viktória Csenge, Roszkopf Léna Adél
     // Előadás: Jó: 10 Elakadt: 4
     // Kinézet: Kiégeti a szemem. 0
     // Funkciók: Jók a slide-ok.: 9
+        // Cicamenhely
 // Kocsi Dorottya, Csorba Eszter Katinka:
     // Kinézet: Tökéletes
     // Elrendezés: Tökéletes
     // Téma: Tökéletes
+        // Kocsmaértékelő, Blog
 // Richter Balázs, Weimper Gergő Zsombor
     // Elrendezés: Jó
-    // // Kinézet: Van mit javítani rajta
+    // Kinézet: Van mit javítani rajta
+        // Call of Duty
+// Szabó Mester Alex
+    // Kinézet: Égeti a szemem, amúgy igényes
+    // Elrendezés: Nem rossz.
+    // Funkciók: Nem rossz.
+        // Portfólió
+// Sass Gyula
+    // Szép.
+        // Portfólió
 
 videoVisible = false
 function toggleVideoVisible(){
     videoVisible = !videoVisible;
     if (videoVisible){
-        document.getElementById("hal").style.display = 'none';
+        let vid = document.getElementById("hal");
+        vid.style.display = 'none';
+        vid.autoplay = true;
+        vid.load();
     }
     else{
-        document.getElementById("hal").style.display = 'block';
+        let vid = document.getElementById("hal");
+        vid.style.display = 'block';
+        vid.autoplay = false;
     }
 }
